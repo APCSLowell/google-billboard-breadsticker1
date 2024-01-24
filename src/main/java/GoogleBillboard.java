1c54f85b97bb966bfa4f9f0e6a0671b38e5aac48
breadsticker1 <143027797+breadsticker1@users.noreply.github.com>
2024-01-24 01:47:28 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,12 +1,11 @@
-
-
 public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435"; 
+
 	public static void main(final String[] args) {
 	for(int i = 2; i < e.length(); i++){
       		String digits = e.substring(i, i+10);
       		double dNum = Double.parseDouble(digits);
-      		if(isPrime(dNum) == true){
+      		if(isPrime(dNum)){
          	System.out.println(dNum);  
       		break;
 		}
@@ -20,7 +19,8 @@ public class GoogleBillboard {
       		return false;
      		for(int i = 2; i <= Math.sqrt(dNum); i++)
         	  if(dNum % i == 0)
-         		return false;
-    	return true;  
+         	    return false;
+    		return true;  
+}
 }
 
